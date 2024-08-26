0163713320099f0d95a7ba511f66ec9f3b3565e7
Samiksha Gaur <92257995+SamGaur@users.noreply.github.com>
2024-08-26 14:27:29 +0530

    Add files via upload
    without turtle lib

---
+++ favorite_color.py
@@ -0,0 +1,9 @@
+# Ask the user for their favorite color
+favorite_color = input("What is your favorite color? ")
+
+# Display the favorite color
+print("Your favorite color is:", favorite_color)
+
+# Display a small color template
+print("Here is your color template:")
+print(f"[ {favorite_color} ]")
